e26dd6fabc18d803f5d552c11fad0a736a49bf7c
RoyDefined <145583009+RoyDefined@users.noreply.github.com>
2023-12-22 16:59:54 +0100

    Add a new map, "Toy Block Tower"

---
+++ edit-toy.ps1
@@ -0,0 +1,17 @@
+. "$PSScriptRoot\scripts\edit-project.ps1"
+
+$wad = "ZombieHordeMaps/toy.wad";
+$map = "FDG65";
+$configuration = "zandronum_DoomUDMF.cfg";
+	
+try
+{
+	editProject -wad $wad -map $map -configuration $configuration;
+}
+catch
+{
+	$message = $_.Exception.Message;
+	Write-Host -ForegroundColor Red "Editing map failed: $message"
+	Read-Host -Prompt "Press any key to exit"
+	Exit
+}
